5dfb431750de8cee1b4e44ba65df7605bccb3766
mmohdkas <mmohdkas@iu.edu>
2020-01-10 17:06:51 -0500

    Initial CRA template

--- src/components/StepIndicator/index.ts
+++
@@ -1,6 +0,0 @@
-/*
-Copyright (C) 2019 The Trustees of Indiana University
-SPDX-License-Identifier: BSD-3-Clause
-*/
-export { default as Step } from './Step';
-export { default as StepIndicator } from './StepIndicator';
