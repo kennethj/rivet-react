2dd9e80fe46cbb1f93f9e135d1a8c1c17c4d89f1
mmohdkas <mmohdkas@iu.edu>
2020-01-15 13:45:52 -0500

    Test removing doc-gen dependency

---
+++ src/components/Badge/Badge.js
@@ -0,0 +1,31 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+//import * as Rivet from '../util/Rivet';
+class Badge extends React.Component {
+    static displayName = 'Badge';
+    static propTypes = {
+        role: PropTypes.oneOf(['secondary']),
+        /**
+         * Optional Rivet style: a secondary badge.
+         */
+        modifier: PropTypes.oneOf(['secondary']),
+        /**
+         * Optional Rivet style: an info/danger/success/warning badge.
+         */
+        variant: PropTypes.oneOf(['info', 'danger', 'success', 'warning'])
+    };
+    render() {
+        const { children, className, modifier, variant, ...attrs } = this.props;
+        const classes = classNames({
+            'rvt-badge': true,
+            [`rvt-badge--${variant}-secondary`]: !!variant && modifier === 'secondary',
+            [`rvt-badge--${variant}`]: !!variant && modifier === undefined,
+            'rvt-badge--secondary': !variant && modifier === 'secondary'
+        }, className);
+        return (
+            <span className={classes} {...attrs}>{children}</span>
+        );
+    }
+}
+export default Badge;
